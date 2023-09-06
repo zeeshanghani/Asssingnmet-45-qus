de632b8317f94955a6fe2bff0e19849a7b466a98
zeeshanghani <mzeeshanghani786@gmail.com>
2023-09-05 23:46:08 -0700

    arry

---
+++ q10.ts
@@ -0,0 +1,9 @@
+//Names: Store the names of a few of your friends in a array called names. 
+//Print each person’s name by accessing each element in the list, one at a time.
+
+var names:string[]=["Mohsin","Adnan","Asif","Mani","Zia",]
+console.log(names[0]);
+console.log(names[1]);
+console.log(names[2]);
+console.log(names[3]);
+console.log(names[4]);
--- q9.js
+++ q9.js
@@ -5,11 +5,12 @@
 //just add your name and the current date at the top of each program file. 
 //Then write one sentence describing what the program does.
 Object.defineProperty(exports, "__esModule", { value: true });
-let whitespace = "   Muhammad Zeeshan  n\";/*white space name ky start mai ", t, use, or;
-n;
-" end mai use krty hain*/ ;
+//let whitespace:string = "\t  Muhammad Zeeshan  n\"; 
+/*white space name ky start mai "\t use or
+\n" end mai use krty hain*/
 console.log("whitespace with Name is =", whitespace);
 let strippedName = whitespace.trim();
 console.log("strippedName is =", strippedName);
+//export{} its use varible error clean whos error same varaible in other file
 // single line commit krny ky lea use hoti hai
 /* */ /* ==> aik sy zyada lines ko ya statement ko commit krny ky lea use krthy hai*/ 
