8ef12f25332386d6beb6581a966d0f66e6b33cec
zeeshanghani <mzeeshanghani786@gmail.com>
2023-09-05 23:25:21 -0700

    comment

--- q9.js
+++ q9.js
@@ -1,16 +1,15 @@
+"use strict";
 //Adding Comments: Choose two of the programs you’ve written, 
 //and add at least one comment to each. If you don’t have anything specific 
 //to write because your programs are too simple at this point, 
 //just add your name and the current date at the top of each program file. 
 //Then write one sentence describing what the program does.
-
-//let whitespace:string = "\t  Muhammad Zeeshan  n\"; 
- /*white space name ky start mai "\t use or 
-\n" end mai use krty hain*/ 
+Object.defineProperty(exports, "__esModule", { value: true });
+let whitespace = "   Muhammad Zeeshan  n\";/*white space name ky start mai ", t, use, or;
+n;
+" end mai use krty hain*/ ;
 console.log("whitespace with Name is =", whitespace);
 let strippedName = whitespace.trim();
 console.log("strippedName is =", strippedName);
-export{}
-//export{} its use varible error clean whos error same varaible in other file
 // single line commit krny ky lea use hoti hai
-/* */ /* ==> aik sy zyada lines ko ya statement ko commit krny ky lea use krthy hai*/
+/* */ /* ==> aik sy zyada lines ko ya statement ko commit krny ky lea use krthy hai*/ 
