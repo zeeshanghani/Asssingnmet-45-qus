589ebdcba5eb080e1f43261986009d2d50e81cbb
Zeeshan Ghani <mzeeshanghani786@gmail.com>
2023-09-06 10:35:28 -0700

    array function

--- q11.js
+++ q11.js
@@ -4,11 +4,13 @@
 // The text of each message should be the same, 
 //but each message should be personalized with the person’s name.
 var names = [`Mohsin`, `Adnan`, `Asif`, `Mani`, `Zia`,];
+//each member solo recive massage
 console.log(`Hello ${names[0]} we go to messive work get ready`);
 // console.log(names[1]);
 // console.log(names[2]);
 // console.log(names[3]);
 // console.log(names[4]);
-for (let name of names) {
+for (let name of names) //use for loop to send massage each member speratorli
+ {
     console.log(`Good Morning ${name}, I hope we done our project soon`);
 }
---
+++ q12.ts
@@ -0,0 +1,10 @@
+/*Your Own Array: Think of your favorite mode of transportation, such as a motorcycle or a car, and make a list that stores several examples. Use your list to print a series of statements about these items, such as “I would like to own a Honda motorcycle.” */
+let favorite_transp:Array < [transport : string, brand:string]>=[]
+favorite_transp.push(["motercycle","honda"])
+favorite_transp.push(["car","tayota"])
+favorite_transp.push(["Bycycle","china"])
+
+console.log(favorite_transp);
+favorite_transp.forEach(([transport,brand]) =>
+{console.log(`I would like to own ${brand} ${transport}.`)}
+)
