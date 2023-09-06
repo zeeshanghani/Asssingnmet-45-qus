962395604cbd28caa46d56f3dd02549b934adf6b
zeeshanghani <mzeeshanghani786@gmail.com>
2023-09-06 00:28:49 -0700

    send msg arry member

---
+++ q11.ts
@@ -0,0 +1,17 @@
+//Greetings: Start with the array you used in Exercise 11,
+// but instead of just printing each person’s name, print a message to them.
+// The text of each message should be the same, 
+//but each message should be personalized with the person’s name.
+
+
+var names:string[]=[`Mohsin`,`Adnan`,`Asif`,`Mani`,`Zia`,]
+//each member solo recive massage
+ console.log(`Hello ${names[0]} we go to messive work get ready`);
+// console.log(names[1]);
+// console.log(names[2]);
+// console.log(names[3]);
+// console.log(names[4]);
+for (let name of names)//use for loop to send massage each member speratorli
+ {
+     console.log(`Good Morning ${name}, I hope we done our project soon`);
+    }
