5b05193b26a07d0d6ed70d0e42f1f1190fe488d0
Zeeshan Ghani <mzeeshanghani786@gmail.com>
2023-09-07 20:50:14 -0700

    invit friend

---
+++ q13.js
@@ -0,0 +1,7 @@
+"use strict";
+//Guest List: If you could invite anyone, living or deceased, to dinner, who would you invite? Make a list that includes at least three people you’d like to invite to dinner. Then use your list to print a message to each person, inviting them to dinner.
+let gestArray = ["Adnan", "Asif", "Zeeshan", "Shafi", "Mani", "Mohsin"];
+//loop for each to print inven
+gestArray.forEach((Nae) => {
+    console.log(`Dear ${Nae},you are invited to dinner . Please join us.`);
+});
---
+++ q13.ts
@@ -0,0 +1,11 @@
+//Guest List: If you could invite anyone, living or deceased, to dinner, who would you invite? Make a list that includes at least three people you’d like to invite to dinner. Then use your list to print a message to each person, inviting them to dinner.
+
+let gestArray:string[]=["Adnan","Asif","Zeeshan","Shafi","Mani","Mohsin"]
+//loop for each to print inven
+gestArray.forEach((Nae)=>
+{
+console.log(`Dear ${Nae},you are invited to dinner . Please join us.`);
+
+
+
+})
