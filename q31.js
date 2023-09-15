17809a09623b15c6937d7d8674540cc30fcdfdd9
Zeeshan Ghani <mzeeshanghani786@gmail.com>
2023-09-15 07:19:14 -0700

    q32 done

---
+++ q31.js
@@ -0,0 +1,24 @@
+// Checking Usernames: Do the following to create a program that simulates how websites ensure that everyone has a unique username.
+// • Make a list of five or more usernames called current_users.
+// • Make another list of five usernames called new_users. Make sure one or two of the new usernames are also in the current_users list.
+// • Loop through the new_users list to see if each new username has already been used. If it has, print a message that the person will need to enter a new username. If a username has not been used, print a message saying that the username is available.
+// • Make sure your comparison is case insensitive. If 'John' has been used, 'JOHN' should not be accepted.
+var curUser = ["admin", "Adnan", "Asif", "Shafi", "Mohsin", "Zeeshan"];
+var newUser = ["mani", "shafi", "Ghani", "zeeshan", "Zaheer",];
+function chkUserName(curUser, newUser) {
+    var lowercaseCurrentUser = curUser.map(user => user.toLowerCase());
+    for (let user of newUser) {
+        var lowercasenewUser = user.toLowerCase();
+        if (lowercaseCurrentUser.includes(lowercasenewUser)) {
+            console.log("usernmae", user, "is alredytaken plz add new user name");
+        }
+        else {
+            console.log("user name", user, "is available");
+        }
+    }
+}
+chkUserName(curUser, newUser);
+export {};
+// var num1:number[]=[1,2,3,4,5,6,7,8,9,10];
+// var num2:number[]=num1.map(i => i*2)
+// console.log(num2);
