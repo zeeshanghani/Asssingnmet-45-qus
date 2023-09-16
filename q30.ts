d3064b95c6d72624b4e7183639508684d35d97d7
Zeeshan Ghani <mzeeshanghani786@gmail.com>
2023-09-16 11:37:38 -0700

    solve

--- q30.ts
+++ q30.ts
@@ -1,3 +1,20 @@
 // No Users: Add an if test to Exercise 28 to make sure the list of users is not empty.
 // • If the list is empty, print the message We need to find some users!
 // • Remove all of the usernames from your array, and make sure the correct message is printed.
+
+var username = ["Admin", "Adnan", "Asif", "Ghani", "Shafi"];
+for (let index = 0; index < username.length; index++) {
+    var element = username[index];
+    if (element == "Admin") {
+        console.log("Hello Admin, would you like to see a status report?");
+    }
+    else {
+        console.log("Hello", element, "thank you for logging in again.");
+    }
+}
+//for empty array
+var userNames = [];
+if (userNames.length === 0) {
+    console.log("We need to find some users!");
+}
+export {};
--- q9.js
+++ q9.js
@@ -1,16 +1,8 @@
-// //Adding Comments: Choose two of the programs you’ve written, 
-// //and add at least one comment to each. If you don’t have anything specific 
-// //to write because your programs are too simple at this point, 
-// //just add your name and the current date at the top of each program file. 
-// //Then write one sentence describing what the program does.
+// Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.
+//We creat a variablw to store favourite personality in it.
+var famousPerson = "Quaid-e-Azam,";
+// we creat another variable to store the qoutation of favourite personality in it.
+var quotation = " ”Before you make a decision… Think a hundred times before you take a decision, but once that decision is taken, stand by it as one man.”";
+//now we cpnsole both variables at once to show thw favourite personality and his qoutation both.
+console.log(famousPerson, "said", quotation);
 export {};
-// //let whitespace:string = "\t  Muhammad Zeeshan  n\"; 
-//  /*white space name ky start mai "\t use or 
-// \n" end mai use krty hain*/ 
-// console.log("whitespace with Name is =", whitespace);
-// let strippedName = whitespace.trim();
-// console.log("strippedName is =", strippedName);
-// export{}
-// //export{} its use varible error clean whos error same varaible in other file
-// // single line commit krny ky lea use hoti hai
-// /* */ /* ==> aik sy zyada lines ko ya statement ko commit krny ky lea use krthy hai*/
